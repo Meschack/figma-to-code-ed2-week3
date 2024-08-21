cd762f5a56fa74bf950cac4655efda8a6c39dcab
Meschack <meshachgbewezoun@gmail.com>
2024-08-21 04:09:16 +0100

    fix(ui): theme switcher

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -3,31 +3,21 @@
 import { useTheme } from 'next-themes'
 
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
 import { Icons } from './common/icons'
 
 export const ModeToggle = () => {
-  const { setTheme } = useTheme()
+  const { setTheme, theme } = useTheme()
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon' className='border-tokena-gray'>
-          <Icons.moon className='rotate-0 scale-100 text-tokena-dark-gray transition-all dark:-rotate-90 dark:scale-0' />
-          <Icons.sun className='absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:text-tokena-gray' />
-          <span className='sr-only'>Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant='outline'
+      size='icon'
+      className='border-tokena-gray'
+      onClick={() => theme && setTheme(theme === 'light' ? 'dark' : 'light')}
+    >
+      <Icons.moon className='rotate-0 scale-100 text-tokena-dark-gray transition-all dark:-rotate-90 dark:scale-0' />
+      <Icons.sun className='absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:text-tokena-gray' />
+      <span className='sr-only'>Toggle theme</span>
+    </Button>
   )
 }
